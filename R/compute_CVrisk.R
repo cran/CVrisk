4fc8c75af73fb36db49e8ff63d76d3931465fbdd
Victor Castro <vcastro@mgh.harvard.edu>
2020-10-27 13:40:07 +0000

    version 1.0.0

---
+++ R/compute_CVrisk.R
@@ -0,0 +1,50 @@
+#' Compute multiple CV risk scores
+#'
+#' @param df input dataframe
+#' @param scores scores to compute, default is all scores
+#' @param age patient age in years (required for all scores)
+#' @param gender patient gender (male or female)
+#' @param race character string for patient race (white, aa, other) column
+#' @param sbp character string of systolic blood pressure (in mm Hg) column
+#' @param bmi character string of Body mass index (kg/m2) column
+#' @param hdl character string of HDL column
+#' @param totchol character string of total cholesterol column
+#' @param bp_med character string of blood pressure medication column
+#' @param smoker character string of smoking status column
+#' @param diabetes character string of diabetes status column
+#'
+#' @return input data frame with risk score results appended as columns
+#'
+#' @examples
+#'
+#' library(CVrisk)
+#' compute_CVrisk(sample_data,
+#'   age = "age", race = "race", gender = "gender", bmi = "BMI", sbp = "sbp",
+#'   hdl = "hdl", totchol = "totchol", bp_med = "bp_med", smoker = "smoker",
+#'   diabetes = "diabetes"
+#' )
+#' @export
+compute_CVrisk <- function(df, scores = c(
+                             "ascvd_10y_accaha", "ascvd_10y_frs",
+                             "ascvd_10y_frs_simple"
+                           ),
+                           age, gender, race, sbp = NULL, bmi = NULL,
+                           hdl = NULL, totchol = NULL,
+                           bp_med = NULL, smoker = NULL, diabetes = NULL) {
+  all_args <- as.list(environment())
+  valid_pred <- c(
+    "age", "gender", "race", "sbp", "bmi", "hdl", "totchol",
+    "bp_med", "smoker", "diabetes"
+  )
+
+  pred_args <- list()
+  for (var in valid_pred) {
+    if (!is.null(all_args[[var]])) pred_args[[var]] <- df[[all_args[[var]]]]
+  }
+
+
+  results <- sapply(scores, function(x) do.call(Vectorize(x), pred_args))
+
+  row.names(results) <- NULL
+  return(cbind(df, results))
+}
---
+++ README.md
@@ -0,0 +1,58 @@
+
+# CVrisk
+
+<!-- badges: start -->
+[![R build status](https://github.com/vcastro/CVrisk/workflows/R-CMD-check/badge.svg)](https://github.com/vcastro/CVrisk/actions)
+[![Codecov test coverage](https://codecov.io/gh/vcastro/CVrisk/branch/master/graph/badge.svg)](https://codecov.io/gh/vcastro/CVrisk?branch=master)
+<!-- badges: end -->
+
+
+## About the package
+
+`CVrisk` is a  package to calculate various cardiovascular disease risk scores. 
+
+### Currently available risk scores
+
+- Framingham 2008 10-year ASCVD risk (model with lipid labs)
+- Framingham 2008 10-year ASCVD risk (model with BMI)
+- ACC/AHA 2013 10-year ASCVD risk 
+
+#### Coming soon
+
+- MESA 2015 10-year ASCVD risk (traditional risk factors)
+- MESA 2015 10-year ASCVD risk (using coronary artery calcium)
+- Reynolds 2007 10-year ASCVD risk in women
+- Framingham 2009 30-year ASCVD risk
+
+## Installation
+
+``` r
+# Install the development version from GitHub:
+# install.packages("devtools")
+devtools::install_github("vcastro/CVRisk")
+```
+
+## Usage
+
+Calculate a single score:
+
+``` r
+library(CVrisk)
+ascvd_10y_accaha(race = "aa", gender = "male", age = 55, 
+   totchol = 213, hdl = 50, sbp = 140, 
+   bp_med = FALSE, smoker=0, diabetes=0)
+   
+```
+
+Calculate multiple scores for a dataframe of data and append scores to the
+dataframe:
+
+``` r
+library(dplyr)
+
+sample_data %>% 
+  compute_CVrisk(age = "age", race = "race", gender = "gender", bmi = "BMI", 
+      sbp = "sbp", hdl = "hdl", totchol = "totchol", bp_med = "bp_med", 
+      smoker = "smoker", diabetes = "diabetes")
+```
+
